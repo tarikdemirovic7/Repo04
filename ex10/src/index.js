09c7555f31a5cc320d22bfa4467e9a1e5dde43f2
TA <TA@TAs-iMac.local>
2021-09-17 00:09:48 +0200

    JavaScript project ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+    //Only change code below this line
+
+var myFirstName = "Tarik"; 
+var mySecondName = "Demirovic"; 
+var myHomeTown = "Sarajevo"; 
+
+    //Only change code above this line
+    return {
+        myFirstName,
+        mySecondName,
+        myHomeTown
+    }; 
+}
+
+console.log(main());
+module.exports = main;
