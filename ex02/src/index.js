0392717f972f9056ba6fd8ccbb528ff04a555a16
TA <TA@TAs-iMac.local>
2021-09-16 22:05:46 +0200

    JavaScript project ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+
+    var myRes = 75; 
+
+    //Only change code below this line
+
+var myRes = 75; 
+myRes++;
+
+    //Only change code above this line
+    return myRes;   
+}
+
+console.log(main());
+
+module.exports = main;
